30387c9f7380aeaa63e380423b65cf943b31e5e3
Arkadiusz Karbowski <126800571+ArkadiuszKarbowski@users.noreply.github.com>
2023-04-08 03:53:24 +0200

    index

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def create_app():
 
     @app.route("/")
     def index():
-      return render_template('index.html')
+        return render_template('index.html')
 
     @app.route("/dodaj")
     def dodaj():
@@ -35,7 +35,7 @@ def create_app():
       except Exception as e:
         db.session.rollback()
         print(str(e))
-      return render_template('test.html')
+      return render_template('index.html')
 
     if __name__ == "__main__":
      app.run(debug=True)
