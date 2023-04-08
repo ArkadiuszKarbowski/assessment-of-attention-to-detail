be1db0d28852861b828ea87c3e0f8285782d0438
S-cichocka <129844379+S-cichocka@users.noreply.github.com>
2023-04-08 22:23:11 +0000

    js update

--- static/js/zad2.js
+++ static/js/zad2.js
@@ -1,57 +1,6 @@
-// Losujemy liczbę od 1 do 4 i tworzymy ścieżkę do pliku z obrazkiem w lewej kolumnie
-const randomNumber = Math.floor(Math.random() * 4) + 1;
-const leftImageSrc = `static/${randomNumber}.png`;
-
-// Tworzymy element obrazka w lewej kolumnie
-const leftImage = document.createElement("img");
-leftImage.src = leftImageSrc;
-
-const leftColumn = document.getElementById("left");
-leftColumn.appendChild(leftImage);
-
-// Tworzymy ścieżkę do pliku z obrazkiem w prawej kolumnie (litery o)
-const rightImageSrc = `static/${randomNumber}o.png`;
-
-// Tworzymy element obrazka w prawej kolumnie
-const rightImage = document.createElement("img");
-rightImage.src = rightImageSrc;
-const rightColumn = document.getElementById("right");
-rightColumn.appendChild(rightImage);
-
-// Tworzymy elementy z odpowiedziami jednokrotnego wyboru
-const answers = [
-  "Odpowiedź 1",
-  "Odpowiedź 2",
-  "Odpowiedź 3",
-  "Odpowiedź 4",
-  "Odpowiedź 5",
-  "Odpowiedź 6",
-];
-const odp = [5,3,1,4];
-const odp_pop = odp[randomNumber];
-const answerList = document.createElement("ul");
-answerList.setAttribute("id", "answer-list");
-
-answers.forEach((answer, index) => {
-  const answerItem = document.createElement("li");
-  const answerRadio = document.createElement("input");
-  answerRadio.setAttribute("type", "radio");
-  answerRadio.setAttribute("name", "answer");
-  answerRadio.setAttribute("value", index + 1);
-
-  answerItem.appendChild(answerRadio);
-  answerItem.appendChild(document.createTextNode(answer));
-  answerList.appendChild(answerItem);
-});
-
-// Dodajemy elementy z odpowiedziami do formularza
-const formContainer = document.getElementById("form-container");
-const answerSection = document.createElement("div");
-answerSection.appendChild(answerList);
-formContainer.insertBefore(answerSection, formContainer.lastChild); // Dodajemy elementy z odpowiedziami przed przyciskiem "Click here"
-
 let czas;
 let selectedAnswer;
+let timeSpent; // zmienna zdefiniowana na początku skryptu
 
 function startTimer() {
   czas = performance.now();
@@ -63,23 +12,18 @@ function stopTimer() {
   console.log(`Czas wykonywania zadania: ${timeSpent} sekund`);
   alert(`Czas wykonywania zadania: ${timeSpent} sekund`);
 
-  // add this line to set the timeSpent variable
-  const timeSpentInput = document.createElement("input");
-  timeSpentInput.setAttribute("type", "hidden");
-  timeSpentInput.setAttribute("name", "timeSpent");
-  timeSpentInput.setAttribute("value", timeSpent);
-  formContainer.appendChild(timeSpentInput);
-}
+  const selectedAnswer = document.querySelector('input[name="answer"]:checked').value;
 
-$.ajax({
-  url: "/page6",
-  type: "POST",
-  data: { ver: randomNumber, correct: odp_pop, timeSpent: timeSpent, sel: selectedAnswer },
-  success: function(response) {
-    console.log(response);
-    window.location.href = "/page7";
-  },
-  error: function(xhr, status, error) {
-    console.log(xhr.responseText);
-  }
-});
+  $.ajax({
+    url: "/page6",
+    type: "POST",
+    data: { ver: randomNumber, correct: odp_pop, timetak: timeSpent, sel: selectedAnswer },
+    success: function(response) {
+      console.log(response);
+      window.location.href = "/page7";
+    },
+    error: function(xhr, status, error) {
+      console.log(xhr.responseText);
+    }
+  });
+}
