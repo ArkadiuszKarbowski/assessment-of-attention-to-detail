6a577b6ae2d1176e1b02a0591f66d0ca78244984
S-cichocka <129844379+S-cichocka@users.noreply.github.com>
2023-04-09 22:14:52 +0000

    changes in ex3

--- static/js/zad3.js
+++ static/js/zad3.js
@@ -1,12 +1,12 @@
 // Tworzenie tablicy z hasłami i ich odbiciami lustrzanymi
-const hasla = ['kajak', 'ala', 'radar', 'potop', 'kajak', 'oko', 'radar', 'potop'];
+const hasla = ['5g@K9zJt', 'x#2Dp8Ls', 'H3qR@t7Z', 'fU6j$W9b', 'N5sT@r2P', 'g#7HkD6c', 'V9nM@p4T', 'e2fL#5tS'];
 const odbicia = hasla.map(haslo => haslo.split('').reverse().join(''));
-
+let correct = 0;
 // Losowe przemieszanie hasel
 for (let i = hasla.length - 1; i > 0; i--) {
-const j = Math.floor(Math.random() * (i + 1));
-[hasla[i], hasla[j]] = [hasla[j], hasla[i]];
-[odbicia[i], odbicia[j]] = [odbicia[j], odbicia[i]];
+  const j = Math.floor(Math.random() * (i + 1));
+  [hasla[i], hasla[j]] = [hasla[j], hasla[i]];
+  [odbicia[i], odbicia[j]] = [odbicia[j], odbicia[i]];
 }
 
 // Tworzenie tabeli i wypełnienie jej danymi
@@ -19,26 +19,39 @@ row.insertCell().innerText = 'Odbicie lustrzane';
 
 const tbody = tabela.createTBody();
 for (let i = 0; i < hasla.length; i++) {
-const row = tbody.insertRow();
-row.insertCell().innerText = (i + 1).toString();
-row.insertCell().innerText = hasla[i];
-row.insertCell().innerText = odbicia[i];
-row.style.border = '1px solid black'; // dodanie obramowania dla komórek
+  const row = tbody.insertRow();
+  row.insertCell().innerText = (i + 1).toString();
+  row.insertCell().innerText = hasla[i];
+  row.insertCell().innerText = odbicia[i];
+  row.style.border = '1px solid black'; // dodanie obramowania dla komórek
+  
+  // Zapisywanie indeksów dla określonych haseł
+  if (hasla[i] === '5g@K9zJt' || hasla[i] === 'fU6j$W9b') {
+    correct = i + ',' ;
+  }
 }
 
 // Tworzenie listy checkboxów
 const checkboxy = document.createElement('div');
-checkboxy.style.marginTop = '20px'; // dodanie odstępu między checkboxami a tabelą
+// Usunięcie losowego znaku z odbicia lustrzanego hasła '5g@K9zJt'
+const index1 = hasla.indexOf('5g@K9zJt');
+const randIndex1 = Math.floor(Math.random() * odbicia[index1].length);
+odbicia[index1].splice(randIndex1, 1);
+
+// Usunięcie losowego znaku z odbicia lustrzanego hasła 'fU6j$W9b'
+const index2 = hasla.indexOf('fU6j$W9b');
+const randIndex2 = Math.floor(Math.random() * odbicia[index2].length);
+odbicia[index2].splice(randIndex2, 1);
 for (let i = 0; i < hasla.length; i++) {
-const checkbox = document.createElement('input');
-checkbox.type = 'checkbox';
-checkbox.value = (i + 1).toString();
-checkbox.style.marginRight = '5px'; // dodanie odstępu między checkboxem a jego etykietą
-const label = document.createElement('label');
-label.innerText = (i + 1).toString();
-checkboxy.appendChild(checkbox);
-checkboxy.appendChild(label);
-checkboxy.appendChild(document.createElement('br')); // dodanie przejścia do nowej linii po każdym checkboxie
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.value = (i + 1).toString();
+  checkbox.style.marginRight = '5px'; // dodanie odstępu między checkboxem a jego etykietą
+  const label = document.createElement('label');
+  label.innerText = (i + 1).toString();
+  checkboxy.appendChild(checkbox);
+  checkboxy.appendChild(label);
+  checkboxy.appendChild(document.createElement('br')); // dodanie przejścia do nowej linii po każdym checkboxie
 }
 
 // Dodanie tabeli i listy checkboxów do dokumentu
@@ -46,6 +59,6 @@ const formContainer = document.getElementById('form-container');
 const leftColumn = document.getElementById('left');
 leftColumn.appendChild(tabela);
 leftColumn.appendChild(document.createElement('br')); // dodanie przejścia do nowej linii między tabelą a listą checkboxów
-leftColumn.appendChild(document.createElement('hr')); // dodanie poziomej linii oddzielającej tabelę od listy checkboxów
-leftColumn.appendChild(document.createElement('h2')).innerText = 'Wybierz hasła'; // dodanie nagłówka nad listą checkboxów
-leftColumn.appendChild(checkboxy);
+const rightColumn = document.getElementById('right');
+rightColumn.appendChild(document.createElement('h2')).innerText = 'Wybierz hasła, których odbicie lustrzane jest błędne';
+rightColumn.appendChild(checkboxy);
