126f525fa4653fd3556223fe57dd533290bca9d0
S-cichocka <129844379+S-cichocka@users.noreply.github.com>
2023-04-10 17:24:19 +0000

    fixed ex4 js

--- static/js/zad4.js
+++ static/js/zad4.js
@@ -3,38 +3,26 @@ var x = Math.floor(Math.random() * 4) + 3;
 
 // Tresci odpowiedzi
 var answers = [
-  "granie na gitarze"
-	"ubranie się",
-	"spacer",
-	"odkurzanie",
-	"pranie",
-	"ugotowanie obiadu",
-	"zmywanie naczyń"
+    "granie na gitarze",
+    "ubranie się",
+    "spacer",
+    "odkurzanie",
+    "pranie",
+    "ugotowanie obiadu",
+    "zmywanie naczyń"
 ];
 
 // Tworzenie pytania wraz z odpowiedziami radiowymi
 var question = "Która z czynności została wykonana jako " + x + "?<br>";
 for (var i = 0; i < answers.length; i++) {
-	question += "<input type='radio' name='answer' value='" + answers[i] + "'>" + answers[i] + "<br>";
+    question += "<input type='radio' name='answer' value='" + answers[i] + "'>" + answers[i] + "<br>";
 }
 
 // Wyświetlanie pytania i odpowiedzi na stronie
-document.getElementById("form-container").innerHTML = question;
+document.getElementById("question-container").innerHTML = question;
 
 // Zapisywanie wybranej odpowiedzi do zmiennej selected
-var selected;
-var radioButtons = document.getElementsByName("answer");
-for (var i = 0; i < radioButtons.length; i++) {
-	if (radioButtons[i].checked) {
-		selectedAnswer = radioButtons[i].value;
-		break;
-	}
-}
-
-let czas;
-let timeSpent; // zmienna zdefiniowana na początku skryptu
-let cor = x-2;
-let correct = answers[cor];
+var selectedAnswer;
 function startTimer() {
   czas = performance.now();
 }
@@ -44,10 +32,15 @@ function stopTimer() {
   const timeSpent = (endTime - czas) / 1000;
   console.log(`Czas wykonywania zadania: ${timeSpent} sekund`);
 
+  selectedAnswer = document.querySelector('input[name="answer"]:checked').value;
+
+  let cor = x - 2;
+  let correct = answers[cor];
+
   $.ajax({
     url: "/page12",
     type: "POST",
-    data: { ver: x, correct: correct, timetak: timeSpent, sel: selectedAnswers },
+    data: { ver: x, correct: correct, timetak: timeSpent, sel: selectedAnswer },
     success: function(response) {
       console.log(response);
       window.location.href = "/page13";
