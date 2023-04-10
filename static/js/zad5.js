42b8a8103cbf7ea7c7c52c6473655e9b5d8b857a
S-cichocka <129844379+S-cichocka@users.noreply.github.com>
2023-04-10 18:50:09 +0000

    fixed error in ex5

--- static/js/zad5.js
+++ static/js/zad5.js
@@ -12,25 +12,27 @@ let selectedAnswer;
 let timeSpent; // zmienna zdefiniowana na początku skryptu
 
 function startTimer() {
-czas = performance.now();
+  czas = performance.now();
 }
 
 function stopTimer() {
-const endTime = performance.now();
-timeSpent = (endTime - czas) / 1000;
-selectedAnswer = document.getElementById("odp").value;
-console.log(Czas wykonywania zadania: ${timeSpent} sekund);
+  const endTime = performance.now();
+  const timeSpent = (endTime - czas) / 1000;
+  console.log(`Czas wykonywania zadania: ${timeSpent} sekund`);
 
-$.ajax({
-url: "/page14",
-type: "POST",
-data: {timetak: timeSpent, sel: selectedAnswer },
-success: function(response) {
-console.log(response);
-window.location.href = "/page15";
-},
-error: function(xhr, status, error) {
-console.log(xhr.responseText);
-}
-});
+  const selectedAnswer = document.querySelector('input[name="answer"]:checked').value;
+
+  $.ajax({
+    $.ajax({
+      url: "/page14",
+      type: "POST",
+      data: {timetak: timeSpent, sel: selectedAnswer },
+      success: function(response) {
+        console.log(response);
+        window.location.href = "/page15";
+      },
+      error: function(xhr, status, error) {
+        console.log(xhr.responseText);
+      }
+  });
 }
