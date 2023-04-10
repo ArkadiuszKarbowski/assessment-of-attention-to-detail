8dde25bab6a3d6328917bb9f18a46fc9395a37ff
S-cichocka <129844379+S-cichocka@users.noreply.github.com>
2023-04-10 00:34:37 +0000

    added user session

--- app.py
+++ app.py
@@ -1,24 +1,17 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 import os
 
 db = SQLAlchemy()
 
-last_user_id = None  # Dodajemy zmienną last_user_id w zakresie globalnym i przypisujemy None
-
-
 def create_app():
   app = Flask(__name__)
+  app.secret_key = 'super_tajny_klucz'
 
   from models.users import Users
   from models.task_results import TestResult
 
-  #coś zmienne srodowiskowe mi nie działają więc
-  #to lokalnie:
-  #app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://attention_to_detail_db_user:DFykAjgLeZjqvCuL5mjtlk9nFXoy1GMf@dpgcgo5pn5269v5rja7nbug-a.frankfurt-postgres.render.com/attention_to_detail_db"
-  #a to na renderze:
   app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('URL_DB')
-
   app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
   db.init_app(app)
@@ -33,7 +26,6 @@ def create_app():
 
   @app.route('/page3', methods=['GET', 'POST'])
   def page3():
-    global last_user_id  # dodajemy global, aby móc modyfikować wartość zmiennej last_user_id
     try:
       if request.method == 'POST':
         first_name = request.form['imie']
@@ -44,47 +36,47 @@ def create_app():
         gender = request.form['plec']
         age = request.form['wiek']
 
-      new_user = Users(first_name=first_name,
-                      last_name=last_name,
-                      study_field=study_field,
-                      university=university,
-                      email=email,
-                      gender=gender,
-                      age=age)
-      db.session.add(new_user)
-      db.session.commit()
-      last_user_id = new_user.id
+        new_user = Users(first_name=first_name,
+                        last_name=last_name,
+                        study_field=study_field,
+                        university=university,
+                        email=email,
+                        gender=gender,
+                        age=age)
+        db.session.add(new_user)
+        db.session.commit()
+        session['user_id'] = new_user.id
     except Exception as e:
       db.session.rollback()
       print(str(e))
     return redirect('/page4')
-  
+
   @app.route('/page4')
   def page4():
     return render_template('spot-typo.html')
-    
+
   @app.route('/page5', methods=['GET', 'POST'])
   def page5():
-    global last_user_id  # dodajemy global, aby móc odczytywać wartość zmiennej last_user_id
     try:
-        if request.method == 'POST':
-          selected_answer = request.form['sum']
-          time_taken = request.form['timetak']
+      user_id = session.get('user_id')
+      if request.method == 'POST':
+        selected_answer = request.form['sum']
+        time_taken = request.form['timetak']
         # Create a new TestResult object and add it to the database
-        new_result = TestResult(user_id=last_user_id, selected_answer=selected_answer, correct_answer=2, task_version=1, time_taken=time_taken, task_number=1)
+        new_result = TestResult(user_id=user_id, selected_answer=selected_answer, correct_answer=2, task_version=1, time_taken=time_taken, task_number=1)
         db.session.add(new_result)
         db.session.commit()
     except Exception as e:
-        db.session.rollback()
-        print(str(e))
+      db.session.rollback()
+      print(str(e))
     return redirect('/page6')
-    
-  @app.route('/page6')
-  def page6():
+
+ @app.route('/page6')
+def page6():
     return render_template('zad2.html')
 
-  @app.route('/page7', methods=['GET', 'POST'])
-  def page7():
+@app.route('/page7', methods=['GET', 'POST'])
+def page7():
     global last_user_id  # dodajemy global, aby móc odczytywać wartość zmiennej last_user_id
     try:
         if request.method == 'POST':
@@ -100,11 +92,13 @@ def create_app():
         db.session.rollback()
         print(str(e))
     return redirect('/page8')
-  @app.route('/page8')
-  def page8():
+
+@app.route('/page8')
+def page8():
     return render_template('zad3.html')
-  @app.route('/page9', methods=['GET', 'POST'])
-  def page9():
+
+@app.route('/page9', methods=['GET', 'POST'])
+def page9():
     global last_user_id  # dodajemy global, aby móc odczytywać wartość zmiennej last_user_id
     try:
         if request.method == 'POST':
@@ -119,10 +113,7 @@ def create_app():
         db.session.rollback()
         print(str(e))
     return redirect('/page10')
-  @app.route('/page10')
-  def page10():
-    return render_template('zad4.html')
-  if __name__ == "__main__":
-    app.run(host= '0.0.0.0', debug=True)
 
-  return app
+@app.route('/page10')
+def page10():
+    return render_template('zad4.html')
