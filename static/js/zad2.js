742581daf11900ef4f18419bf3db196706d0d0d8
Arkadiusz Karbowski <126800571+ArkadiuszKarbowski@users.noreply.github.com>
2023-04-11 20:23:25 +0200

    pages till 6 are work fine now

--- static/js/zad2.js
+++ static/js/zad2.js
@@ -1,78 +1,79 @@
-const randomNumber = Math.floor(Math.random() * 4) + 1;
-const leftImageSrc = `static/${randomNumber}.png`;
+  const randomNumber = Math.floor(Math.random() * 4) + 1;
+  const leftImageSrc = `static/${randomNumber}.png`;
 
-// Tworzymy element obrazka w lewej kolumnie
-const leftImage = document.createElement("img");
-leftImage.src = leftImageSrc;
+  // Tworzymy element obrazka w lewej kolumnie
+  const leftImage = document.createElement("img");
+  leftImage.src = leftImageSrc;
+  leftImage.classList.add("image-container"); // Dodajemy klasę "image-container"
+  const leftColumn = document.getElementById("left");
+  leftColumn.appendChild(leftImage);
 
-const leftColumn = document.getElementById("left");
-leftColumn.appendChild(leftImage);
+  // Tworzymy ścieżkę do pliku z obrazkiem w prawej kolumnie (litery o)
+  const rightImageSrc = `static/${randomNumber}o.png`;
 
-// Tworzymy ścieżkę do pliku z obrazkiem w prawej kolumnie (litery o)
-const rightImageSrc = `static/${randomNumber}o.png`;
+  // Tworzymy element obrazka w prawej kolumnie
+  const rightImage = document.createElement("img");
+  rightImage.src = rightImageSrc;
+  rightImage.classList.add("image-container"); // Dodajemy klasę "image-container"
+  const rightColumn = document.getElementById("right");
+  rightColumn.appendChild(rightImage);
 
-// Tworzymy element obrazka w prawej kolumnie
-const rightImage = document.createElement("img");
-rightImage.src = rightImageSrc;
-const rightColumn = document.getElementById("right");
-rightColumn.appendChild(rightImage);
+  // Tworzymy elementy z odpowiedziami jednokrotnego wyboru
+  const answers = [
+    "Odpowiedź 1",
+    "Odpowiedź 2",
+    "Odpowiedź 3",
+    "Odpowiedź 4",
+    "Odpowiedź 5",
+    "Odpowiedź 6",
+  ];
+  const odp = [5,3,1,4];
+  const odp_pop = odp[randomNumber - 1];
+  const answerList = document.createElement("ul");
+  answerList.setAttribute("id", "answer-list");
 
-// Tworzymy elementy z odpowiedziami jednokrotnego wyboru
-const answers = [
-  "Odpowiedź 1",
-  "Odpowiedź 2",
-  "Odpowiedź 3",
-  "Odpowiedź 4",
-  "Odpowiedź 5",
-  "Odpowiedź 6",
-];
-const odp = [5,3,1,4];
-const odp_pop = odp[randomNumber - 1];
-const answerList = document.createElement("ul");
-answerList.setAttribute("id", "answer-list");
+  answers.forEach((answer, index) => {
+    const answerItem = document.createElement("li");
+    const answerRadio = document.createElement("input");
+    answerRadio.setAttribute("type", "radio");
+    answerRadio.setAttribute("name", "answer");
+    answerRadio.setAttribute("value", index + 1);
 
-answers.forEach((answer, index) => {
-  const answerItem = document.createElement("li");
-  const answerRadio = document.createElement("input");
-  answerRadio.setAttribute("type", "radio");
-  answerRadio.setAttribute("name", "answer");
-  answerRadio.setAttribute("value", index + 1);
-
-  answerItem.appendChild(answerRadio);
-  answerItem.appendChild(document.createTextNode(answer));
-  answerList.appendChild(answerItem);
-});
-
-// Dodajemy elementy z odpowiedziami do formularza
-const formContainer = document.getElementById("form-container");
-const answerSection = document.createElement("div");
-answerSection.appendChild(answerList);
-formContainer.insertBefore(answerSection, formContainer.lastChild); // Dodajemy elementy z odpowiedziami przed przyciskiem "Click here"
-let czas;
-let selectedAnswer;
-let timeSpent; // zmienna zdefiniowana na początku skryptu
-
-function startTimer() {
-  czas = performance.now();
-}
-
-function stopTimer() {
-  const endTime = performance.now();
-  const timeSpent = (endTime - czas) / 1000;
-  console.log(`Czas wykonywania zadania: ${timeSpent} sekund`);
-
-  const selectedAnswer = document.querySelector('input[name="answer"]:checked').value;
-
-  $.ajax({
-    url: "/page7",
-    type: "POST",
-    data: { ver: randomNumber, correct: odp_pop, timetak: timeSpent, sel: selectedAnswer },
-    success: function(response) {
-      console.log(response);
-      window.location.href = "/page8";
-    },
-    error: function(xhr, status, error) {
-      console.log(xhr.responseText);
-    }
+    answerItem.appendChild(answerRadio);
+    answerItem.appendChild(document.createTextNode(answer));
+    answerList.appendChild(answerItem);
   });
-}
+
+  // Dodajemy elementy z odpowiedziami do formularza
+  const formContainer = document.getElementById("form-container");
+  const answerSection = document.createElement("div");
+  answerSection.appendChild(answerList);
+  formContainer.insertBefore(answerSection, formContainer.lastChild); // Dodajemy elementy z odpowiedziami przed przyciskiem "Click here"
+  let czas;
+  let selectedAnswer;
+  let timeSpent; // zmienna zdefiniowana na początku skryptu
+
+  function startTimer() {
+    czas = performance.now();
+  }
+
+  function stopTimer() {
+    const endTime = performance.now();
+    const timeSpent = (endTime - czas) / 1000;
+    console.log(`Czas wykonywania zadania: ${timeSpent} sekund`);
+
+    const selectedAnswer = document.querySelector('input[name="answer"]:checked').value;
+
+    $.ajax({
+      url: "/page7",
+      type: "POST",
+      data: { ver: randomNumber, correct: odp_pop, timetak: timeSpent, sel: selectedAnswer },
+      success: function(response) {
+        console.log(response);
+        window.location.href = "/page8";
+      },
+      error: function(xhr, status, error) {
+        console.log(xhr.responseText);
+      }
+    });
+  }
