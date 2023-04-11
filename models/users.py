0b1bdd4e6a4f13b11d49db07038c603e521cf09a
S-cichocka <129844379+S-cichocka@users.noreply.github.com>
2023-04-11 23:55:41 +0000

    modified db

--- models/users.py
+++ models/users.py
@@ -2,8 +2,6 @@ from app import db
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(80), nullable=False)
-    last_name = db.Column(db.String(80), nullable=False)
     study_field = db.Column(db.String(80), nullable=False)
     university = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120))
