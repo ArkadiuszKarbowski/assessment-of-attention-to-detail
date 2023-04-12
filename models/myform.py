02e09fe2b52de94a56c839c173d58651addaaa4c
S-cichocka <129844379+S-cichocka@users.noreply.github.com>
2023-04-12 00:06:34 +0000

    modified db v3

--- models/myform.py
+++ models/myform.py
@@ -4,8 +4,6 @@ from wtforms.validators import DataRequired, Email, Length, Optional, InputRequi
 
 
 class AnkietaForm(FlaskForm):
-    first_name  = StringField('Imię', validators=[DataRequired(), Length(max=15)])
-    last_name  = StringField('Nazwisko', validators=[DataRequired(), Length(max=40)])
     study_program  = StringField('Kierunek studiów', validators=[DataRequired()])
     university  = StringField('Uczelnia', validators=[DataRequired()])
     email = StringField('Email', validators=[Optional(), Email()])
