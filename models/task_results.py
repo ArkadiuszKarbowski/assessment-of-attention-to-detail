0d3d54f4fbe561f50c2ab6b1dbe196aaa6721d3d
S-cichocka <129844379+S-cichocka@users.noreply.github.com>
2023-04-17 21:37:55 +0200

    Update task_results.py

--- models/task_results.py
+++ models/task_results.py
@@ -5,11 +5,10 @@ class TestResult(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    selected_answer = db.Column(db.Text, nullable=False)
-    correct_answer = db.Column(db.Text, nullable=False)
-    task_version = db.Column(db.Integer, nullable=False)
-    time_taken = db.Column(db.Integer, nullable=False)
-    task_number = db.Column(db.Integer)
+    selected_answer = db.Column(db.Text)
+    correct_answer = db.Column(db.Text)
+    task_version = db.Column(db.Integer)
+    time_taken = db.Column(db.Integer)
     
     def __repr__(self):
         return f'<TestResult id={self.id}, user_id={self.user_id},  task_version={self.task_version}, time_taken={self.time_taken}, timestamp={self.timestamp}, task_number={self.task_number}>'
